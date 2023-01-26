a7b89e4cd7c3b335ed2b81d17b99f38f7991b152
Devon Terry <114770792+crumbycupid@users.noreply.github.com>
2023-01-26 02:46:30 -0500

    big wrappers

--- src/main/java/com/devonSongr/helloWorld/Album.java
+++ src/main/java/com/devonSongr/helloWorld/Album.java
@@ -3,8 +3,8 @@ package com.devonSongr.helloWorld;
 public class Album {
     private String title;
     private String artist;
-    private int songCount;
-    private int length;
+    private Integer songCount;
+    private Integer length;
     private String imageURL;
 
     public Album(String title, String artist, int songCount, int length, String imageURL){
